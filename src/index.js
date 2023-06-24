a01fa596f354dc79b43be3ca57da5e7cd441e046
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-06-23 20:23:46 -0700

    basic express application

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import app from "./app.js";
 dotenv.config();
 
 // environment variables
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
