f074e1f83784c1b5f4dde010e3736c5ff6c3656e
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-06-23 20:24:38 -0700

    basic express application

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ const app = express();
 
 // routes
 app.get("/", (req, res) =>
-  res.send("The Server is here! Where is your content?!")
+  res.send("The Server is here with Get! Where is your content?!")
 );
 
 app.post("/test", (req, res) =>
-  res.send("The Server is here! Where is your content?!")
+  res.send("The Server is here with POSt! Where is your content?!")
 );
 
 export default app;
